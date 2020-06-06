65c18d88155a6917b2fc023d008f3de830fbe6bc
prana <prakashrana9847067614@gmail.com>
2020-06-06 11:09:16 -0400

    Added register UI

--- script/register.js
+++ script/register.js
@@ -1,74 +1,83 @@
 
 
-
-$( document ).ready(function() {
-
-    //listener for register button
-    $("#register-btn").click(function (e) { 
+$(document).ready(function () {
+    
+    $("button[type=submit]").click(function (e) { 
         e.preventDefault();
-        registerUser();
-    });
+       
 
+        //ge the datas
+
+        let email = $("#email").val();
+        console.log(email);
+
+        let firstname = $("#firstname").val();
+        console.log(firstname);
+
+        let lastname = $("#lastname").val();
+        console.log(lastname);
+
+        let password = $("#password").val();
+        console.log(password);
+
+        let repassword = $("#repassword").val();
+        console.log(repassword);
+
+
+        if(password != repassword){
+            alert("Both Passwords are not same.")
+            return
+        }
+
+        let dob = $("#dob").val();
+        console.log(dob);
+
+        let gender =$("input:radio[name ='gender']:checked").val();
+        console.log(gender);
+
+    
+        let hashPasswordDict = hashPassword(password)
+        let hashedPassword = hashPasswordDict.hash;
+        let salt = hashPasswordDict.salt;
+        
+
+
+        let ob = {
+            email: email,
+            firstname: firstname,
+            lastname: lastname,
+            password: hashedPassword,
+            salt:salt,
+            dob:dob,
+            gender: gender,
+        };
+        // Save to venue table
+
+        let trans = db.transaction([DB_USER_STORE], 'readwrite');
+        let addReq = trans.objectStore(DB_USER_STORE).add(ob);
+
+        addReq.onerror = function(e) {
+            console.log('error storing data');
+            console.error(e);
+        }
+
+        trans.oncomplete = function(e) {
+            console.log('data stored');
+        }
+
+        // get the db access ---------------------->
+        
+
+    });
 });
 
 
 
 
 
-var readUserInput = function(){
-    let email = $("#email").val();
-    let password = $("#psw").val();
-    let dob = $("#dob").val();
-    let gender = $("input[name='gender']:checked").val();
-
-    let hashPasswordDict = hashPassword(password)
-    let hashedPassword = hashPasswordDict.hash;
-    let salt = hashPasswordDict.salt;
-    return {email: email, password: hashedPassword, dob: dob, gender: gender, salt: salt}
-
-}
 
 
 
-var registerUser = function(version, currentCallback){
-    console.log("openDb ...");
-
-    var db;
-    // open the db
-    var req = indexedDB.open(DB_NAME, DB_VERSION);
-
-    // On success assigne the db variable 
-    req.onsuccess = function (evt) {
-        // Equal to: db = req.result;
-        db = req.result;
-        var tx = db.transaction(DB_USER_STORE,"readwrite");
-        var store = tx.objectStore(DB_USER_STORE);
-        
-        db.onerror = function(e){
-                console.log("ERROR "+ e.target.errorCode);
-        }
-    
-        data = readUserInput();
-        store.put(data);
-        tx.oncomplete = function () {
-            db.close();
-          };
-    
-        
-    };
-
-    // On error simply console for debug
-    req.onerror = function (evt) {
-        console.error("openDb:", evt.target.errorCode);
-    };
-
-    // create tables for the first time
-    req.onupgradeneeded = function (evt) {
-        console.log("openDb.onupgradeneeded");
-        var store = evt.currentTarget.result.createObjectStore(
-        DB_USER_STORE, { keyPath: 'email', autoIncrement: true });
-    };
 
 
-}
 
