1cd8a65cc97f26d8d8f6474704f6bee9cd9d1ad3
prana <prakashrana9847067614@gmail.com>
2020-06-06 10:11:54 -0400

    Added code to display messages

--- script/profile.js
+++ script/profile.js
@@ -36,14 +36,43 @@ $(document).ready(function () {
         }
 
 
+
+        // update messages
+        let trans1 = db.transaction([DB_MESSAGES_STORE], 'readwrite');
+        let req1 = trans1.objectStore(DB_MESSAGES_STORE).openCursor();
+
+        req1.onerror = function(e) {
+            console.log('error reading data');
+            console.error(e);
+        }
+
+        req1.onsuccess = function(event) {
+            let cursor = event.target.result;
+            if (cursor) {
+                let key = cursor.primaryKey;
+                let value = cursor.value;
+                if(value.email == email){
+                    console.log(key, value);
+                }
+                addMessagesToLister(value);
+                cursor.continue();
+            }
+            else {
+                // no more results
+               
+               
+               
+            }
+         };
+
+         req1.oncomplete = function(e) {
+            console.log('data read');
+            
+        }
+
+
         function addBookingsToLister(venueObj){
 
-
-          
-    
-            
-           
-    
             var txt = 
             `<div class="result">
            
@@ -75,7 +104,74 @@ $(document).ready(function () {
             </div>
                 
         </div>`
-        $(txt).appendTo(".lister");
+        $(txt).appendTo("#booking-lister");
+        }
+
+        function addMessagesToLister(messageObj){
+
+            var txt = 
+            `<div class="result">
+           
+    
+            <div class="detail-container">
+               
+                <div class="lower-deck">
+                    <div class="first-type">
+                        Name:
+                        <div class="first-name">`+
+                        messageObj.name+
+                        `</div>
+                    </div>
+    
+                    <div class="second-type">
+                        <i></i>
+                        title:
+                        <div class="second-name">`+
+                        messageObj.subject + `
+                        </div>
+                    </div>
+
+                    <div class="third-type">
+                        <i></i>
+                        Date:
+                        <div class="third-name">`+
+                        messageObj.sentDate + `
+                        </div>
+                    </div>
+    
+                    
+    
+                    
+    
+                </div>
+
+                <div class="lower-deck">
+                <div class="first-type">
+                    Desc:
+                    <div class="first-name">`+
+                    messageObj.description+
+                    `</div>
+                </div>
+                </div>
+
+
+                <div class="lower-deck">
+                <div class="first-type">
+                    Desc:
+                    <div class="first-name">`+
+                    messageObj.email+
+                    `</div>
+                </div>
+                </div>
+
+
+
+            </div>
+    
+            </div>
+                
+        </div>`
+        $(txt).appendTo("#messages-lister");
         }
 
 });
