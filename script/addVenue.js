aced2c00a04e9490a68fca59d61b23f2ec4638b5
prana <prakashrana9847067614@gmail.com>
2020-06-06 03:34:23 -0400

    Added code for venue submission

---
+++ script/addVenue.js
@@ -0,0 +1,92 @@
+$(document).ready(function () {
+    
+    $("button[type=submit]").click(function (e) { 
+        e.preventDefault();
+       
+
+        //ge the datas
+
+        let venueName = $("#venue-name").val();
+        console.log(venueName);
+
+        let address = $("#address").val();
+        console.log(address);
+
+        let pincode = $("#pincode").val();
+        console.log(pincode);
+
+        let city = $("#city").val();
+        console.log(city);
+
+        let state = $("#state").val();
+        console.log(state);
+
+        let description = $("#description").val();
+        console.log(description);
+
+        let sports = $("#multiselect").val();
+        console.log(sports);
+
+        let images = $("#image-upload-input").prop('files');
+        console.log(images);
+
+        
+
+        for(let i= 0; i< images.length ; i++){
+
+            var reader = new FileReader();
+            let element = images[i];
+        
+            reader.readAsBinaryString(element);
+
+            reader.onload = function(e) {
+                //alert(e.target.result);
+                let bits = e.target.result;
+                let ob = {
+                    venueName: venueName,
+                    data:bits
+                };
+        
+                let trans = db.transaction([DB_IMAGE_STORE], 'readwrite');
+                let addReq = trans.objectStore(DB_IMAGE_STORE).add(ob);
+        
+                addReq.onerror = function(e) {
+                    console.log('error storing data');
+                    console.error(e);
+                }
+        
+                trans.oncomplete = function(e) {
+                    console.log('data stored');
+                }
+            }
+
+        }
+
+        let ob = {
+            venueName: venueName,
+            address: address,
+            pincode: pincode,
+            city: city,
+            state: state,
+            description:description,
+            sports: sports,
+        };
+        // Save to venue table
+
+        let trans = db.transaction([DB_VENUE_STORE], 'readwrite');
+        let addReq = trans.objectStore(DB_VENUE_STORE).add(ob);
+
+        addReq.onerror = function(e) {
+            console.log('error storing data');
+            console.error(e);
+        }
+
+        trans.oncomplete = function(e) {
+            console.log('data stored');
+        }
+
+        // get the db access ---------------------->
+        
+
+    });
+});
--- script/db.js
+++ script/db.js
@@ -8,86 +8,136 @@ if (!window.indexedDB) {
 const DB_NAME = 'bobal-play-storage';
 const DB_VERSION = 1; 
 const DB_USER_STORE = 'users'
-// var db;
-var openDB = function(version, currentCallback){
-    console.log("openDb ...");
-
-    var db;
-    // open the db
-    var req = indexedDB.open(DB_NAME, DB_VERSION);
-
-    // On success assigne the db variable 
-    req.onsuccess = function (evt) {
-      // Equal to: db = req.result;
-      db = req.result;
-      console.log("openDb DONE");
-      currentCallback(db);
-    };
-
-    // On error simply console for debug
-    req.onerror = function (evt) {
-      console.error("openDb:", evt.target.errorCode);
-    };
-
-    // create tables for the first time
-    req.onupgradeneeded = function (evt) {
-      console.log("openDb.onupgradeneeded");
-      var store = evt.currentTarget.result.createObjectStore(
-        DB_USER_STORE, { keyPath: 'email', autoIncrement: true });
-    };
+const DB_IMAGE_STORE = 'images'
+const DB_VENUE_STORE = 'venues'
 
 
-}
 
+var db;
 
-// inserts given data in given storeName
+// open the db
+var req = indexedDB.open(DB_NAME, DB_VERSION);
 
-
-var insertData = function(storeName, data){
+// On success assigne the db variable 
+req.onsuccess = function (evt) {
+    // Equal to: db = req.result;
+    db = evt.target.result;
+    console.log('db opened');
     
-    var insertCallBack = function(db){
-        var tx = db.transaction(storeName,"readwrite");
-        var store = tx.objectStore(storeName);
+};
+
+// On error simply console for debug
+req.onerror = function (evt) {
+    console.error("openDb:", evt.target.errorCode);
+};
+
+// create tables for the first time
+req.onupgradeneeded = function (evt) {
+    console.log("openDb.onupgradeneeded");
+    var store = evt.currentTarget.result.createObjectStore(
+    DB_USER_STORE, { keyPath: 'email', autoIncrement: true });
+
+    var imageStore = evt.currentTarget.result.createObjectStore(
+      DB_IMAGE_STORE, {  autoIncrement: true });
+
+
+    var venueStore = evt.currentTarget.result.createObjectStore(
+      DB_VENUE_STORE, {  keyPath: 'venueName' ,autoIncrement: true });
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var openDB = function(version, currentCallback){
+//     console.log("openDb ...");
+
+//     var db;
+//     // open the db
+//     var req = indexedDB.open(DB_NAME, DB_VERSION);
+
+//     // On success assigne the db variable 
+//     req.onsuccess = function (evt) {
+//       // Equal to: db = req.result;
+//       db = req.result;
+//       console.log("openDb DONE");
+//       currentCallback(db);
+//     };
+
+//     // On error simply console for debug
+//     req.onerror = function (evt) {
+//       console.error("openDb:", evt.target.errorCode);
+//     };
+
+//     // create tables for the first time
+//     req.onupgradeneeded = function (evt) {
+//       console.log("openDb.onupgradeneeded");
+//       var store = evt.currentTarget.result.createObjectStore(
+//         DB_USER_STORE, { keyPath: 'email', autoIncrement: true });
+//     };
+
+
+// }
+
+
+// // inserts given data in given storeName
+
+
+// var insertData = function(storeName, data){
+    
+//     var insertCallBack = function(db){
+//         var tx = db.transaction(storeName,"readwrite");
+//         var store = tx.objectStore(storeName);
         
-        db.onerror = function(e){
-                console.log("ERROR "+ e.target.errorCode);
-        }
+//         db.onerror = function(e){
+//                 console.log("ERROR "+ e.target.errorCode);
+//         }
     
-        store.put(data);
-        tx.oncomplete = function () {
-            db.close();
-          };
+//         store.put(data);
+//         tx.oncomplete = function () {
+//             db.close();
+//           };
     
     
-    }
-    openDB(DB_VERSION,insertCallBack);
-}
+//     }
+//     openDB(DB_VERSION,insertCallBack);
+// }
 
 
 
-var readData = function(storeName, key){
+// var readData = function(storeName, key){
     
-    var readCallBack = function(db){
-        var tx = db.transaction(storeName,"readwrite");
-        var store = tx.objectStore(storeName);
+//     var readCallBack = function(db){
+//         var tx = db.transaction(storeName,"readwrite");
+//         var store = tx.objectStore(storeName);
         
-        db.onerror = function(e){
-                console.log("ERROR "+ e.target.errorCode);
-        }
+//         db.onerror = function(e){
+//                 console.log("ERROR "+ e.target.errorCode);
+//         }
     
-        let q = store.get(key);
+//         let q = store.get(key);
         
-        q.onsuccess = function(){
-            return q.result;
-        }
+//         q.onsuccess = function(){
+//             return q.result;
+//         }
 
-        tx.oncomplete = function () {
-            db.close();
-          };
+//         tx.oncomplete = function () {
+//             db.close();
+//           };
     
     
-    }
-    openDB(DB_VERSION,readCallBack);
-}
+//     }
+//     openDB(DB_VERSION,readCallBack);
+// }
 
 
