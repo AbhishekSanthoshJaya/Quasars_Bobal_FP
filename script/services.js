18dae3f08565baa920a66206a4f871ea0dfe6938
prana <prakashrana9847067614@gmail.com>
2020-06-06 04:49:50 -0400

    code to read results from db

--- script/services.js
+++ script/services.js
@@ -1,4 +1,45 @@
 $(document).ready(function () {
     checkLoginCookie();
 
+    $(".search button").click(function (e) { 
+        e.preventDefault();
+        console.log("clicked");
+
+
+        // read from database
+
+        let trans = db.transaction([DB_VENUE_STORE], 'readwrite');
+        let req = trans.objectStore(DB_VENUE_STORE).openCursor();
+
+        req.onerror = function(e) {
+            console.log('error reading data');
+            console.error(e);
+        }
+
+        req.onsuccess = function(event) {
+            let cursor = event.target.result;
+            if (cursor) {
+                let key = cursor.primaryKey;
+                let value = cursor.value;
+                console.log(key, value);
+                addResultToLister(value);
+                cursor.continue();
+            }
+            else {
+                // no more results
+            }
+         };
+
+        req.oncomplete = function(e) {
+            console.log('data read');
+        }
+        
+    });
+
+
+    function addResultToLister(venueObj){
+        $(".lister").append('<div class="result">'+venueObj.venueName+'</div>');
+
+    }
+
 });
